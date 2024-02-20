ae7aa3bd51667e758cc09ed351b275d45e6e6fc1
Gupptag123 <160593570+Gupptag123@users.noreply.github.com>
2024-02-20 16:00:09 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+int reverseDigits(int num) 
+{ 
+    int rev_num = 0; 
+    while (num > 0) { 
+        rev_num = rev_num * 10 + num % 10; 
+        num = num / 10; 
+    } 
+    return rev_num; 
+} 
+int main() 
+{ 
+    int num = 356; 
+    cout << "Reverse of num is " << reverseDigits(num); 
+    getchar(); 
+    return 0; 
+}
